0ba77e42b7cc1c5932cc7d4dc26d13069b1c9d66
wfrules <37941044@qq.com>
2018-09-02 23:36:32 +0800

    文件预览实现

--- app/Admin/Controllers/VodController.php
+++ app/Admin/Controllers/VodController.php
@@ -73,10 +73,21 @@ class VodController extends Controller
     {
         return Admin::grid(Vod::class, function (Grid $grid) {
 
-            $grid->id('ID')->sortable();
-
-            $grid->created_at();
-            $grid->updated_at();
+//            $grid->id('ID')->sortable();
+//
+//            $grid->created_at();
+//            $grid->updated_at();
+            $grid->title();
+            $grid->pic_path()->display(function ($pic_path) {
+                $sFilePath = "http://file.debug.com/" . $pic_path;
+                return "<img style='width: 200px;height: 150px' src='$sFilePath'></img>";
+            });;
+            $grid->pageurl()->display(function ($pageurl) {
+                return  "<a href='$pageurl' target='_blank'>查看详情</a>";
+            });;
+            $grid->vodurl()->display(function ($vodUrl) {
+                return "<a href='$vodUrl' target='_blank'>播放</a>";
+            });
         });
     }
 
